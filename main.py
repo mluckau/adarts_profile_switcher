a2eed7cf5915accc184f0d24e7c87ba328663658
Michael <mluckau@gmail.com>
2024-12-23 00:29:47 +0100

    debug reduce

--- main.py
+++ main.py
@@ -177,8 +177,8 @@ directory = './config'
 if not os.path.exists(directory):
     os.makedirs(directory)
     print(f"Directory '{directory}' created.")
-else:
-    print(f"Directory '{directory}' already exists.")
+# else:
+    # print(f"Directory '{directory}' already exists.")
 
 if not os.path.exists('./config/key.key'):
     key = Fernet.generate_key()
